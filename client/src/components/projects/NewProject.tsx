7a2fd48936777e01a67d40ff7e17caa1e9345fb7
tommy-js <tylerb814925@hotmail.com>
2020-05-17 10:53:36 -0400

    Edits to New Project page

--- client/src/components/projects/NewProject.tsx
+++ client/src/components/projects/NewProject.tsx
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar } from "../navigation/Navbar";
 import plus from "../../icons/plus.png";
 import deleted from "../../icons/delete.png";
 
 export const NewProject: React.FC = () => {
   const [tech, setTech] = useState([{ key: 0, input: "" }]);
+  const [positions, setPositions] = useState([{ key: 0, input: "" }]);
+  const [checked, setChecked] = useState(false);
+  const [visible, setVisible] = useState("none");
+
+  useEffect(() => {
+    if (checked) {
+      setVisible("inline-block");
+    } else {
+      setVisible("none");
+    }
+  }, [checked]);
 
   function addTech() {
     let newTech = { key: Math.floor(Math.random() * 1000000), input: "" };
@@ -13,7 +24,7 @@ export const NewProject: React.FC = () => {
     setTech(oldTech);
   }
 
-  function castValue(e: any, id: number) {
+  function castTechValue(e: any, id: number) {
     let foundItem: any = tech.find((el: any) => el.key === id);
     let originTech = [...tech];
     let index = originTech.indexOf(foundItem);
@@ -27,10 +38,67 @@ export const NewProject: React.FC = () => {
     setTech(filteredTech);
   }
 
+  function addPosition() {
+    let newPos = { key: Math.floor(Math.random() * 1000000), input: "" };
+    let oldPos = [...positions];
+    oldPos.push(newPos);
+    setPositions(oldPos);
+  }
+
+  function castPosValue(e: any, id: number) {
+    let foundItem: any = positions.find((el: any) => el.key === id);
+    let originPosition = [...positions];
+    let index = originPosition.indexOf(foundItem);
+    originPosition[index].input = e.target.value;
+    setPositions(originPosition);
+  }
+
+  function removePosition(key: number) {
+    let removingPosition = [...positions];
+    let filteredPosition = removingPosition.filter(el => el.key != key);
+    setPositions(filteredPosition);
+  }
+
+  function openPositions() {
+    if (checked) {
+      return (
+        <div>
+          {positions.map(el => (
+            <div className="new_tech_inputs">
+              <input
+                className="inner_new_tech"
+                key={el.key}
+                value={el.input}
+                onChange={e => castPosValue(e, el.key)}
+                placeholder="position"
+              />
+              <div
+                className="delete_button"
+                onClick={() => removePosition(el.key)}
+              >
+                <img className="delete_button_image" src={deleted} />
+              </div>
+            </div>
+          ))}
+          <p className="appear_on_hover positions_appear">
+            Specific positions you're looking to fill...
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <p className="appear_on_hover positions_appear">
+          General positions filled so far...
+        </p>
+      );
+    }
+  }
+
   return (
     <div>
       <Navbar />
       <div className="new_project_block">
+        <h2 style={{ color: "pink" }}>New Project</h2>
         <div className="project_title_block">
           <input className="new_project_title" placeholder="Title" />
           <p className="appear_on_hover title_appear">
@@ -43,7 +111,9 @@ export const NewProject: React.FC = () => {
             placeholder="Description"
           />
           <p className="appear_on_hover description_appear">
-            Describe your project to us
+            Describe your project to us. Include information such as expected
+            timeline, technologies used, size of the project, and development
+            pace.
           </p>
         </div>
         <div className="new_project_stack_block">
@@ -63,7 +133,7 @@ export const NewProject: React.FC = () => {
                 className="inner_new_tech"
                 key={el.key}
                 value={el.input}
-                onChange={e => castValue(e, el.key)}
+                onChange={e => castTechValue(e, el.key)}
                 placeholder="technology"
               />
               <div className="delete_button" onClick={() => removeTech(el.key)}>
@@ -74,6 +144,24 @@ export const NewProject: React.FC = () => {
           <p className="appear_on_hover tech_appear">
             Tell us about the technologies you're using for this project
           </p>
+        </div>
+        <div className="new_project_positions_block">
+          <h2>Enter number of currently filled positions</h2>
+          <h4>
+            Toggle to show whether you're looking for generalists or specialists
+          </h4>
+          <div className="positions_open">
+            <input className="new_project_stack" placeholder="0/n" />
+            <input type="checkbox" onChange={() => setChecked(!checked)} />
+            <div
+              className="add_tech_button"
+              style={{ display: visible }}
+              onClick={() => addPosition()}
+            >
+              <img className="add_tech_icon" src={plus} />
+            </div>
+          </div>
+          {openPositions()}
         </div>
       </div>
     </div>
