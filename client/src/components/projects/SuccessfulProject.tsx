6daf3fbffc2a5858383a937dc4ee9b7b1e389251
tommy-js <tylerb814925@hotmail.com>
2020-05-21 22:48:49 -0400

    Changes to SuccessfulProject component

--- client/src/components/projects/SuccessfulProject.tsx
+++ client/src/components/projects/SuccessfulProject.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { userContext } from "../../App";
+import { Link } from "react-router-dom";
+import "../../App.scss";
 
 export const SuccessfulProject: React.FC = () => {
+  const { userVal, setUserVal } = useContext(userContext);
   return (
-    <div>
-      <p>Thanks for submitting!</p>
+    <div className="successful_project_block">
+      <h2 className="successful_project_thanks">Thanks for submitting!</h2>
+      <button>Back to profile</button>
+
+      <div className="new_project_button">
+        <Link to={`../profile/projects/${userVal.id}`}>
+          <span className="hide_on_swipe">Back to profile</span>
+          <div className="swipe_right">
+            <span className="show_on_swipe">Back to profile</span>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
